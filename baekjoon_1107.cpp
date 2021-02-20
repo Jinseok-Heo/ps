6ba6d43c51641c2fbfb9b22fc10ccd1eb88e11b5
Jinseok-Heo <71974878+Jinseok-Heo@users.noreply.github.com>
2021-02-20 12:45:41 +0900

    Update baekjoon_1107.cpp

--- baekjoon_1107.cpp
+++ baekjoon_1107.cpp
@@ -9,48 +9,48 @@
 
 using namespace std;
 
-void input_data(int* goal, int* n_broken, vector<bool>& botton);
-int countPressed(int goal, vector<bool>& botton);
-int findMinMove(int goal, vector<bool>& botton);
+void input_data(int* goal, int* n_broken, vector<bool>& button);
+int countPressed(int goal, vector<bool>& button);
+int findMinMove(int goal, vector<bool>& button);
 
 int main(){
     ios_base:: sync_with_stdio(false);
     cin.tie(NULL);
     int goal, n_broken;
-    vector<bool> botton(10, true);
-    input_data(&goal, &n_broken, botton);
-    cout << findMinMove(goal, botton) << "\n";
+    vector<bool> button(10, true);
+    input_data(&goal, &n_broken, button);
+    cout << findMinMove(goal, button) << "\n";
     return 0;
 }
 
-void input_data(int* goal, int* n_broken, vector<bool>& botton) {
+void input_data(int* goal, int* n_broken, vector<bool>& button) {
     cin >> *goal;
     cin >> *n_broken;
     for(int i = 0; i < *n_broken; i++) {
         int brokenBtn;
         cin >> brokenBtn;
-        botton[brokenBtn] = false;
+        button[brokenBtn] = false;
     }
 }
 
-int countPressed(int goal, vector<bool>& botton) {
+int countPressed(int goal, vector<bool>& button) {
     if(!goal) {
-        if(botton[0]) return 1;
+        if(button[0]) return 1;
         else          return 0;
     }
     int cnt = 0;
     while(goal > 0) {
-        if(!botton[goal % 10]) return 0;
+        if(!button[goal % 10]) return 0;
         cnt++;
         goal /= 10;
     }
     return cnt;
 }
 
-int findMinMove(int goal, vector<bool>& botton) {
+int findMinMove(int goal, vector<bool>& button) {
     int minMove = abs(goal - CUR_CHANNEL);
     for(int i = 0; i < 1000000; i++) {
-        int cntPressed = countPressed(i, botton);
+        int cntPressed = countPressed(i, button);
         if(cntPressed) {
             int move = abs(i - goal) + cntPressed;
             minMove = minMove < move ? minMove : move;
