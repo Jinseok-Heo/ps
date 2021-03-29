6bc5168dba599a20933a2f980ff19553d4c41b6f
Jinseok-Heo <71974878+Jinseok-Heo@users.noreply.github.com>
2021-03-29 13:17:10 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <deque>
+#include <iomanip>
+#include <functional>
+#include <unordered_map>
+#define p pair<ll, ll>
+#define ll long long
+#define INF 1000000000
+
+using namespace std;
+
+void inputData(int *n, int *m, vector<vector<int>> &distInfo);
+void Floyd_Warshall(vector<vector<int>> &distInfo);
+
+int main() {
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    vector<vector<int>> distInfo;
+    inputData(&n, &m, distInfo);
+
+    Floyd_Warshall(distInfo);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) cout << distInfo[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+void inputData(int *n, int *m, vector<vector<int>> &distInfo) {
+    cin >> *n;
+    cin >> *m;
+    distInfo.resize(*n, vector<int>(*n, INF));
+    for(int i = 0; i < *m; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        distInfo[x - 1][y - 1] = distInfo[x - 1][y - 1] < z ? distInfo[x - 1][y - 1] : z;
+    }
+}
+
+void Floyd_Warshall(vector<vector<int>> &dist) {
+    int n = dist.size();
+    for(int i = 0; i < n; i++) {
+        dist[i][i] = 0;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j == i) continue;
+            for(int k = 0; k < n; k++) {
+                if(k == i || j == k) continue;
+                if(dist[j][k] > dist[j][i] + dist[i][k]) {
+                    cout << dist[j][k] << " " << dist[j][i] << " " << dist[i][k] << "\n";
+                    dist[j][k] = dist[j][i] + dist[i][k];
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] == INF) dist[i][j] = 0;
+        }
+    }
+}
